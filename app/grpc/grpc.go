9be26065fcde385e45c27a5dbeb3ed59cfadcb72
x <x@x.com>
2022-07-12 12:27:52 +0800

    2022-7-12 12:27

--- app/grpc/grpc.go
+++ app/grpc/grpc.go
@@ -5,6 +5,7 @@ import (
 	"tmpl-go-vercel/app/pingpong"
 	pingpongpb "tmpl-go-vercel/gen/go/api/pingpong/v1"
 
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"google.golang.org/grpc"
 )
 
@@ -13,10 +14,10 @@ type Handler http.Handler
 func New() Handler {
 	s := grpc.NewServer()
 	pingpongpb.RegisterPingPongServiceServer(s, &pingpong.Service{})
-	// return grpcweb.WrapServer(s, grpcweb.WithOriginFunc(func(origin string) bool {
-	// 	// Allow all origins, DO NOT do this in production
-	// 	return true
-	// }), grpcweb.WithCorsForRegisteredEndpointsOnly(false), grpcweb.WithAllowedRequestHeaders([]string{"*"}))
+	return grpcweb.WrapServer(s, grpcweb.WithOriginFunc(func(origin string) bool {
+		// Allow all origins, DO NOT do this in production
+		return true
+	}), grpcweb.WithCorsForRegisteredEndpointsOnly(false), grpcweb.WithAllowedRequestHeaders([]string{"*"}))
 
-	return s
+	// return s
 }
